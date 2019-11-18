da4a87bfb222fe0ddec9398c751f62169af1bf7e
kellycoles <kellycoles1@gmail.com>
2019-11-18 11:03:02 -0600

    ticket 9 complete

--- BangazonWorkforce/BangazonWorkforce/Models/TrainingProgram.cs
+++ BangazonWorkforce/BangazonWorkforce/Models/TrainingProgram.cs
@@ -9,6 +9,7 @@ namespace BangazonWorkforce.Models
     public class TrainingProgram
     {
         public int Id{ get; set; }
+        [Display(Name = "Training Programs")]
         public string Name { get; set; }
         [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
